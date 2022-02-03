14d8bff3fa7cc44c61b9d6453ab6facc23e112d0
ukalelei <uvioletl@hotmail.com>
2022-02-03 12:22:55 -0600

    fill square with random color

--- canvas.js
+++ canvas.js
@@ -2,5 +2,10 @@ let canvas = document.querySelector("canvas");
 let context = canvas.getContext("2d");
 
 //make square
-context.fillStyle = "pink";
+context.fillStyle = "hsl(" + 360 * Math.random() + ", 50%, 50%)";
 context.fillRect(150, 150, 300, 300);
+
+//change color on click
+function changeColor(){
+    
+}
