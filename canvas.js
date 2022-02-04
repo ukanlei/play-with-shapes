6970c99be42ae24a3046872d090191f7130ac5a1
ukalelei <uvioletl@hotmail.com>
2022-02-04 13:36:30 -0600

    change color of square on each click

--- canvas.js
+++ canvas.js
@@ -1,11 +1,30 @@
 let canvas = document.querySelector("canvas");
 let context = canvas.getContext("2d");
+let randomColor = "hsl(" + 360 * Math.random() + ", 50%, 50%)";
 
 //make square
-context.fillStyle = "hsl(" + 360 * Math.random() + ", 50%, 50%)";
+context.fillStyle = randomColor;
 context.fillRect(150, 150, 300, 300);
 
-//change color on click
-function changeColor(){
-    
+//new filled square
+function newColorSquare(){
+    let randomColor = "hsl(" + 360 * Math.random() + ", 50%, 50%)";
+
+    //erase original square
+    context.clearRect(150, 150, 300, 300); 
+
+    //redraw square
+    context.beginPath(); //start new path
+    context.rect(150, 150, 300, 300); //set dimension of square
+    context.fillStyle = randomColor; //set color
+    context.fill(); //fill square
 }
+
+document.addEventListener("click", newColorSquare); //detect click then newColorSquare()
+
+
+
+
+
+
+
