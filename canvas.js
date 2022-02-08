4646067b0f058d0f520615ee6658bf6ed4f7d843
ukalelei <uvioletl@hotmail.com>
2022-02-08 14:39:54 -0600

    create function that will make a filled circle

--- canvas.js
+++ canvas.js
@@ -4,11 +4,11 @@ let randomColor = "hsl(" + 360 * Math.random() + ", 50%, 50%)";
 
 //make square
 context.fillStyle = randomColor;
-context.fillRect(150, 150, 300, 300);
+context.fillRect(150, 150, 300, 300);*/
 
 //new filled square
 function newColorSquare(){
-    let randomColor = "hsl(" + 360 * Math.random() + ", 50%, 50%)";
+    randomColor = "hsl(" + 360 * Math.random() + ", 50%, 50%)";
 
     //erase original square
     context.clearRect(150, 150, 300, 300); 
@@ -16,6 +16,15 @@ function newColorSquare(){
     //redraw square
     context.beginPath(); //start new path
     context.rect(150, 150, 300, 300); //set dimension of square
+    context.fillStyle = randomColor; //set color
+    context.fill(); //fill square
+}
+
+//Create filled circle
+function filledCircle(){
+    //draw circle
+    context.beginPath(); //start new path
+    context.arc(300,300,150,0, Math.PI*2);
     context.fillStyle = randomColor; //set color
     context.fill(); //fill square
 }
