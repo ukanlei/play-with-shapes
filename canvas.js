5c11f5d433fafbc1270b89f1f71dd51c73495194
ukalelei <uvioletl@hotmail.com>
2022-02-10 12:57:34 -0600

    square replaced with circle when circle button is clicked

--- canvas.js
+++ canvas.js
@@ -4,7 +4,8 @@ let randomColor = "hsl(" + 360 * Math.random() + ", 50%, 50%)";
 
 //make square
 context.fillStyle = randomColor;
-context.fillRect(150, 150, 300, 300);*/
+context.fillRect(150, 150, 300, 300);
+
 
 //new filled square
 function newColorSquare(){
@@ -22,12 +23,18 @@ function newColorSquare(){
 
 //Create filled circle
 function filledCircle(){
+
+    context.clearRect(0, 0, 600, 600);
     //draw circle
     context.beginPath(); //start new path
     context.arc(300,300,150,0, Math.PI*2);
     context.fillStyle = randomColor; //set color
     context.fill(); //fill square
 }
+
+let circleBtn = document.getElementById("CircleBtn");
+circleBtn.addEventListener("click", filledCircle)
+
 
 let shape = document.getElementById("shapeCanvas");
 shape.addEventListener("click", newColorSquare); //detect click then newColorSquare()
